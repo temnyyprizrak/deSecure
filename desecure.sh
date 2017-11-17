e367e98aaa4b610fdfc39c8889b83837c755e858
temnyyprizrak <temnyyprizrak@protonmail.com>
2017-11-17 13:03:36 -0800

    Added first few lines

--- desecure.sh
+++ desecure.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# First we upgrade the system
+sudo apt update
+sudo apt upgrade
+
+# Now we install the necessary tools needed
+sudo apt install macchanger tor torify torsocks pidgin pidgin-otr
