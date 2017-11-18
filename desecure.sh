eb88629c175b6da861f64cb9f3a5ca61799fe699
temnyyprizrak <temnyyprizrak@protonmail.com>
2017-11-17 16:18:35 -0800

    Added new features

--- desecure.sh
+++ desecure.sh
@@ -5,11 +5,17 @@ sudo apt update
 sudo apt upgrade
 
 # Now we install the necessary tools needed
-sudo apt install macchanger tor torify torsocks pidgin pidgin-otr
+sudo apt install macchanger tor torify torsocks pidgin pidgin-otr nmap
 
 # Change MAC address
 echo 'Changing MAC address...'
 sudo ifconfig wlp3s0 down
 sudo macchanger -r wlp3s0
 sudo ifconfig wlp3s0 up
+
+echo 'Assessing open ports...'
+nmap -Pn 127.0.0.1
+echo 'Close any ports you may with iptables'
+
+
 
